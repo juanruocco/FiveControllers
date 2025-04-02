6116dde29b31d5550d2499193614c57e13e58889
juanruocco <juanpabloruocco@gmail.com>
2025-04-01 21:53:03 -0500

    code for 4 buttons 5in1 and 2 joysticks

--- 5Joysticks1Hand/5Joysticks1Hand.ino
+++ 5Joysticks1Hand/5Joysticks1Hand.ino
@@ -9,13 +9,22 @@
 //BleGamepadConfiguration bleGamepadConfig;  
 
 XboxGamepadDevice *gamepad;
-BleCompositeHID compositeHID("ESP32 SeriesX Controller", "Mystfit", 100);
+//BleCompositeHID compositeHID("Multi Left", "Jota", 100);
+//BleCompositeHID compositeHID("Multi Der", "Jota", 100);
+BleCompositeHID compositeHID("Multi Test", "Jota", 100);
 
 //BleKeyboard Keyboard("FiveInOne", "Bluetooth Device Manufacturer", 100);
 
 //BleGamepad bleGamepad("RO1", "jota", 100);
 
+const int NORTH_DIRE = 135;
+const int SOUTH_DIRE = 136;
+const int WEST_DIRE = 137;
+const int EAST_DIRE = 138;
+const int TRIGER_LEFT  = 141;
+const int TRIGER_RIGHT = 142;
 
+//JOYSTICK VARIABLES
 const int xJoystick1 = 25;
 const int yJoystick1 = 26;
 bool leftJoystickPressed1 = false;
@@ -30,6 +39,7 @@ bool rigthJoystickPressed2 = false;
 bool upJoystickPressed2 = false;
 bool downJoystickPressed2 = false;
 
+/*
 const int xJoystick3 = 25;
 const int yJoystick3 = 26;
 bool leftJoystickPressed3 = false;
@@ -50,12 +60,28 @@ bool leftJoystickPressed5 = false;
 bool rigthJoystickPressed5 = false;
 bool upJoystickPressed5 = false;
 bool downJoystickPressed5 = false;
+*/
 
 int range = 32767;  // output range of X or Y movement
 int minRange = 0;
 int maxRange = 32767;
 int threshold = 100;//range / 6;  // resting threshold
 int centerCalibration = 1700;
+
+
+//BUTTONS VARIABLES
+int buttonRow[5] = { 17, 18,  4,  5, 16 };  //Input //{ 17, 18, 4, 5 , 16 }; //{ 4, 16, 17, 5, 18 };
+int buttonCol[4] = { 21, 23, 22,  19     };  //Output
+
+boolean keyboardLettersPressed[5][4] = { false };
+
+uint16_t gamepadButtons[5][4] =   { 
+                                    { NORTH_DIRE      , XBOX_BUTTON_LS          , XBOX_BUTTON_Y               , XBOX_BUTTON_RS          },                                 
+                                    { EAST_DIRE       , XBOX_BUTTON_SELECT      , XBOX_BUTTON_B               , XBOX_BUTTON_SHARE       },     
+                                    { SOUTH_DIRE      , XBOX_BUTTON_LB          , XBOX_BUTTON_A               , XBOX_BUTTON_RB          },        
+                                    { WEST_DIRE       , 1                       , XBOX_BUTTON_X               , XBOX_BUTTON_START       },    
+                                    { TRIGER_LEFT     , XBOX_BUTTON_HOME        , TRIGER_RIGHT                , 1                       }                             
+                                  };
 
 void setup() {
   Serial.begin(115200);
@@ -81,15 +107,36 @@ void setup() {
 
   FunctionSlot<XboxGamepadOutputReportData> vibrationSlot(OnVibrateEvent);
   gamepad->onVibrate.attach(vibrationSlot);
+  
   compositeHID.addDevice(gamepad);
   Serial.println("Starting composite HID device...");
   compositeHID.begin(hostConfig);
 
 
+
+  //Setup Switch
+  pinMode(buttonRow[0], INPUT_PULLUP);
+  pinMode(buttonRow[1], INPUT_PULLUP);
+  pinMode(buttonRow[2], INPUT_PULLUP);
+  pinMode(buttonRow[3], INPUT_PULLUP);
+  pinMode(buttonRow[4], INPUT_PULLUP);
+  
+  pinMode(buttonCol[0], OUTPUT);
+  pinMode(buttonCol[1], OUTPUT);
+  pinMode(buttonCol[2], OUTPUT);
+  pinMode(buttonCol[3], OUTPUT);
+
+  digitalWrite(buttonCol[0], HIGH);
+  digitalWrite(buttonCol[1], HIGH);
+  digitalWrite(buttonCol[2], HIGH);
+  
+
   delay(300);
-  centerCalibration = analogRead(xJoystick1);
-  gamepad->setRightThumb(0, 0);
+  gamepad->setLeftThumb(1, 1);
+  gamepad->setRightThumb(1, 1);
   gamepad->sendGamepadReport();
+  centerCalibration = analogRead(xJoystick1);
+  
 }
 
 void OnVibrateEvent(XboxGamepadOutputReportData data)
@@ -105,8 +152,8 @@ void OnVibrateEvent(XboxGamepadOutputReportData data)
 int readAxis(int thisAxis, boolean inverse) {
 
   int reading = analogRead(thisAxis);
-  Serial.print("analogue: ");
-  Serial.print(reading);
+  //Serial.print("analogue: ");
+  //Serial.print(reading);
   int distance = reading - centerCalibration;
 
   if (abs(distance) < threshold) {
@@ -114,8 +161,8 @@ int readAxis(int thisAxis, boolean inverse) {
   }
 
   int defaultCalibration = 4096/2 - centerCalibration;
-  Serial.print(" , calibra: ");
-  Serial.print(defaultCalibration);
+  //Serial.print(" , calibra: ");
+  //Serial.print(defaultCalibration);
 
   int output =  centerCalibration + distance + defaultCalibration;
   if(inverse == true){
@@ -130,42 +177,118 @@ int readAxis(int thisAxis, boolean inverse) {
     output = 0;
   }
 
-  
-
-  Serial.print(" , output: ");
-  Serial.print(output);
-
+  //Serial.print(" , output: ");
+  //Serial.print(output);
 
   reading = map(output, 0, 4096, -range, range);
-  Serial.print(", convert: ");
-  Serial.print(reading);
-  Serial.print(" , ");
+  //Serial.print(", convert: ");
+  //Serial.print(reading);
+  //Serial.print(" , ");
   
-
-  
-
   return reading;
 }
 
 
-void joystickDirection(int xJoyPin, int yJoyPin){
+void joystickDirection(int xJoyPinLeft, int yJoyPinLeft, int xJoyPinRight, int yJoyPinRight){
 
-  int xReading = readAxis(xJoyPin, true);
-  int yReading = readAxis(yJoyPin, false);
-  //int yReading = 0;
-  Serial.print("x: ");
+  int xReading = readAxis(xJoyPinLeft, true);
+  int yReading = readAxis(yJoyPinLeft, false);
+  int xReadingRigth = readAxis(xJoyPinRight, true);
+  int yReadingRigth = readAxis(yJoyPinRight, false);
+ /* 
+  Serial.print("xl: ");
   Serial.print(xReading);
-  Serial.print(", y: ");
-  Serial.println(yReading);
+  Serial.print(", yl: ");
+  Serial.print(yReading);
+  Serial.print(", xr: ");
+  Serial.print(xReadingRigth);
+  Serial.print(", yr: ");
+  Serial.println(yReadingRigth);
+*/
   if(compositeHID.isConnected()){
-    Serial.print(", connected ");
+    //Serial.print(", connected ");
     gamepad->setLeftThumb (xReading, yReading);
-    gamepad->setRightThumb(1, 1);
+    gamepad->setRightThumb(xReadingRigth, yReadingRigth);
     gamepad->sendGamepadReport();
   }
   
-  delay(20);
+  delay(10);
 }
+
+
+//PRESS GAMEPAD BUTTONS
+void pressButtons(int row, int col){
+  
+  Serial.print("PRESS  : ");
+  Serial.print(gamepadButtons[row][col]);
+  Serial.print(", Row: ");
+  Serial.print(row);
+  Serial.print(" ,Col: ");
+  Serial.println(col);
+  //XBOX_BUTTON_DPAD_NORTH
+  if( gamepadButtons[row][col] == NORTH_DIRE ){
+    gamepad->pressDPadDirectionFlag(XboxDpadFlags::NORTH);
+  }else if(gamepadButtons[row][col] == SOUTH_DIRE){
+    gamepad->pressDPadDirectionFlag(XboxDpadFlags::SOUTH);
+  }else if(gamepadButtons[row][col] == WEST_DIRE){
+    gamepad->pressDPadDirectionFlag(XboxDpadFlags::WEST);
+  }else if(gamepadButtons[row][col] == EAST_DIRE){
+    gamepad->pressDPadDirectionFlag(XboxDpadFlags::EAST);
+  }else if(gamepadButtons[row][col] == TRIGER_LEFT){
+    gamepad-> setLeftTrigger(30000);
+  }else if(gamepadButtons[row][col] == TRIGER_RIGHT){
+    gamepad-> setRightTrigger(30000);
+  }else{
+    gamepad->press(gamepadButtons[row][col]);
+  }
+  
+  gamepad->sendGamepadReport();
+
+  /*
+  if( keyboardLettersPressed[0][0]  ){  // XBOX_BUTTON_DPAD_NORTH 
+      gamepad->pressDPadDirectionFlag(XboxDpadFlags::NORTH);
+    }else if(gamepadButtons[1][0] ){  // XBOX_BUTTON_DPAD_SOUTH
+      gamepad->pressDPadDirectionFlag(XboxDpadFlags::SOUTH);
+    }else if(gamepadButtons[2][0] ){  // == XBOX_BUTTON_DPAD_WEST
+      gamepad->pressDPadDirectionFlag(XboxDpadFlags::WEST);
+    }else if(gamepadButtons[3][0] ){  // == XBOX_BUTTON_DPAD_EAST
+      gamepad->pressDPadDirectionFlag(XboxDpadFlags::EAST);
+    }else{
+      gamepad->press(gamepadButtons[row][col]);
+    }
+  */
+}  
+
+void unpressButtons(int row, int col){
+  
+  Serial.print("UNPRESS: ");
+  Serial.print(gamepadButtons[row][col]);
+  Serial.print(", Row: ");
+  Serial.print(row);
+  Serial.print(" ,Col: ");
+  Serial.println(col);
+
+  if( keyboardLettersPressed[0][0]  && gamepadButtons[row][col] == NORTH_DIRE){ 
+      gamepad  -> releaseDPad();
+  }else if(gamepadButtons[1][0] && gamepadButtons[row][col] == SOUTH_DIRE){  
+      gamepad  -> releaseDPad();
+  }else if(gamepadButtons[2][0] && gamepadButtons[row][col] == WEST_DIRE){  
+      gamepad  -> releaseDPad();
+  }else if(gamepadButtons[3][0] && gamepadButtons[row][col] == EAST_DIRE){  
+      gamepad  -> releaseDPad();
+  }else if(gamepadButtons[0][1] && gamepadButtons[row][col] == TRIGER_LEFT){  
+      gamepad  -> setLeftTrigger(0);
+      gamepad  -> setRightTrigger(0);
+  }else if(gamepadButtons[0][3] && gamepadButtons[row][col] == TRIGER_RIGHT){  
+      gamepad  -> setLeftTrigger(0);
+      gamepad  -> setRightTrigger(0);
+  }else{//Release button
+      gamepad->release(gamepadButtons[row][col]);
+  }
+
+  gamepad->sendGamepadReport();
+}
+
 
 /*void joystickDirection(int xJoyPin, int yJoyPin){
 
@@ -180,9 +303,7 @@ void joystickDirection(int xJoyPin, int yJoyPin){
   {
     bleGamepad.setLeftThumb(xReading, yReading);
   }
-  
 }*/
-
 
 /*void joystickToKeyboardDirection(int xJoyPin, int yJoyPin, int leftKey, int rightKey, int upKey, int downKey, bool &leftPressed, bool &rightPressed, bool &upPressed, bool &downPressed){
 
@@ -228,11 +349,41 @@ void joystickDirection(int xJoyPin, int yJoyPin){
   }
 }*/
 
+void keyboardDetection(){
+
+  //Keyboard Detection
+  for (int col = 0; col < 4; col++) {
+    digitalWrite(buttonCol[col], LOW);
+    delay(1);
+    for (int row = 0; row < 5; row++) {
+      int buttonState = digitalRead(buttonRow[row]);
+      if (buttonState == LOW) {
+        if (keyboardLettersPressed[row][col] == false) {
+          keyboardLettersPressed[row][col] = true;
+          pressButtons(row, col);
+        }
+      } else {
+        if (keyboardLettersPressed[row][col] == true) {
+          unpressButtons(row, col);
+        }
+        keyboardLettersPressed[row][col] = false;
+      }
+    }
+    
+    //delay(5000);
+    digitalWrite(buttonCol[col], HIGH);
+    delay(1);
+  }
+}
+
 void loop() {
   
   //joystickDirection1();
-  joystickDirection(xJoystick1, yJoystick1);
-  delay(5);
+  keyboardDetection();
+  joystickDirection(xJoystick1, yJoystick1, xJoystick2, yJoystick2);
+  delay(2);
+
+
   //joystickDirection(xJoystick2, yJoystick2, 'f', 'h', 't', 'g', leftJoystickPressed2, rigthJoystickPressed2, upJoystickPressed2, downJoystickPressed2);
   //delay(5);
   //joystickDirection(xJoystick3, yJoystick3, 'j', 'l', 'i', 'k', leftJoystickPressed3, rigthJoystickPressed3, upJoystickPressed3, downJoystickPressed3);

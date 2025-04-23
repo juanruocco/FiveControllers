28f534b0c9c5455b7787ef001143b3de07859bbe
juanruocco <juanpabloruocco@gmail.com>
2025-04-23 00:40:43 -0500

    Added the esp com example

--- 5Joysticks1Hand/5Joysticks1Hand.ino
+++ 5Joysticks1Hand/5Joysticks1Hand.ino
@@ -7,12 +7,11 @@ uint8_t MAC_P2[] = {0x64, 0xE8, 0x33, 0x7E, 0x04, 0x3C};
 uint8_t MAC_P3[] = {0xA0, 0x85, 0xE3, 0xE7, 0x44, 0x28};  
 uint8_t MAC_P4[] = {0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC}; 
 
-//#include "ESP32_NOW.h"
+//ESP LIBRARIES
+#include "ESP32_NOW.h"
 #include "WiFi.h"
-#include <esp_now.h>
-#include <esp_wifi.h>
 #include <esp_mac.h>  // For the MAC2STR and MACSTR macros
-#include <esp_system.h>
+#include <vector>
 
 #include <BleConnectionStatus.h>
 #include <BleCompositeHID.h>
@@ -21,14 +20,191 @@ uint8_t MAC_P4[] = {0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC};
 #include "JoystickDirection.h"
 #include "JoystickButton.h"
 
-#define ESPNOW_WIFI_CHANNEL 6
+#define ESPNOW_WIFI_IFACE WIFI_IF_STA // Wi-Fi interface to be used by the ESP-NOW protocol
+#define ESPNOW_WIFI_CHANNEL 4         // Channel to be used by the ESP-NOW protocol
+#define ESPNOW_SEND_INTERVAL_MS 100  // Delay between sending messages
+#define ESPNOW_PEER_COUNT 1           // Number of peers to wait for (excluding this device)
+#define REPORT_INTERVAL 5             // Report to other devices every 5 messages
+
+#define ESPNOW_EXAMPLE_PMK "pmk1234567890123" // Primary Master Key (PMK)
+#define ESPNOW_EXAMPLE_LMK "lmk1234567890123" // Local Master Key (LMK)
+
+typedef struct struct_message {
+    int idPlayer;       // ID del dispositivo que envía (ej: 1, 2, 3)
+    boolean isLeftSide;  // Algún valor de ejemplo
+    boolean isUpSide;  // Algún valor de ejemplo
+    int value1;
+    int value2;
+} struct_message;
+
+typedef struct {
+  uint32_t count;
+  uint32_t priority;
+  uint32_t data;
+  bool ready;
+  char str[7];
+} __attribute__((packed)) esp_now_data_t;
+
+/* Global Variables */
+uint32_t self_priority = 0;          // Priority of this device
+uint8_t current_peer_count = 0;      // Number of peers that have been found
+bool device_is_master = false;       // Flag to indicate if this device is the master
+bool master_decided = false;         // Flag to indicate if the master has been decided
+uint32_t sent_msg_count = 0;         // Counter for the messages sent. Only starts counting after all peers have been found
+uint32_t recv_msg_count = 0;         // Counter for the messages received. Only starts counting after all peers have been found
+esp_now_data_t new_msg;              // Message that will be sent to the peers
+std::vector<uint32_t> last_data(5);  // Vector that will store the last 5 data received
+
+class ESP_NOW_Network_Peer : public ESP_NOW_Peer {
+public:
+  uint32_t priority;
+  bool peer_is_master = false;
+  bool peer_ready = false;
+
+  ESP_NOW_Network_Peer(const uint8_t *mac_addr, uint32_t priority = 0, const uint8_t *lmk = (const uint8_t *)ESPNOW_EXAMPLE_LMK)
+    : ESP_NOW_Peer(mac_addr, ESPNOW_WIFI_CHANNEL, ESPNOW_WIFI_IFACE, lmk), priority(priority) {}
+
+  ~ESP_NOW_Network_Peer() {}
+
+  bool begin() {
+    // In this example the ESP-NOW protocol will already be initialized as we require it to receive broadcast messages.
+    if (!add()) {
+      log_e("Failed to initialize ESP-NOW or register the peer");
+      return false;
+    }
+    return true;
+  }
+
+  bool send_message(const uint8_t *data, size_t len) {
+    if (data == NULL || len == 0) {
+      log_e("Data to be sent is NULL or has a length of 0");
+      return false;
+    }
+
+    // Call the parent class method to send the data
+    return send(data, len);
+  }
+
+  void onReceive(const uint8_t *data, size_t len, bool broadcast) {
+    esp_now_data_t *msg = (esp_now_data_t *)data;
+
+    if (peer_ready == false && msg->ready == true) {
+      Serial.printf("Peer " MACSTR " reported ready\n", MAC2STR(addr()));
+      peer_ready = true;
+    }
+
+    if (!broadcast) {
+      recv_msg_count++;
+      if (device_is_master) {
+        Serial.printf("Received a message from peer " MACSTR "\n", MAC2STR(addr()));
+        Serial.printf("  Count: %lu\n", msg->count);
+        Serial.printf("  Random data: %lu\n", msg->data);
+        last_data.push_back(msg->data);
+        last_data.erase(last_data.begin());
+      } else if (peer_is_master) {
+        Serial.println("Received a message from the master");
+        Serial.printf("  Average data: %lu\n", msg->data);
+      } else {
+        Serial.printf("Peer " MACSTR " says: %s\n", MAC2STR(addr()), msg->str);
+      }
+    }
+  }
+
+  void onSent(bool success) {
+    bool broadcast = memcmp(addr(), ESP_NOW.BROADCAST_ADDR, ESP_NOW_ETH_ALEN) == 0;
+    if (broadcast) {
+      log_i("Broadcast message reported as sent %s", success ? "successfully" : "unsuccessfully");
+    } else {
+      log_i("Unicast message reported as sent %s to peer " MACSTR, success ? "successfully" : "unsuccessfully", MAC2STR(addr()));
+    }
+  }
+};
+
+/* Peers */
+
+std::vector<ESP_NOW_Network_Peer *> peers;                             // Create a vector to store the peer pointers
+ESP_NOW_Network_Peer broadcast_peer(ESP_NOW.BROADCAST_ADDR, 0, NULL);  // Register the broadcast peer (no encryption support for the broadcast address)
+ESP_NOW_Network_Peer *master_peer = nullptr;                           // Pointer to peer that is the master
+
+/* Helper functions */
+
+// Function to reboot the device
+void fail_reboot() {
+  Serial.println("Rebooting in 5 seconds...");
+  delay(5000);
+  ESP.restart();
+}
+
+// Function to check which device has the highest priority
+uint32_t check_highest_priority() {
+  uint32_t highest_priority = 0;
+  for (auto &peer : peers) {
+    if (peer->priority > highest_priority) {
+      highest_priority = peer->priority;
+    }
+  }
+  return std::max(highest_priority, self_priority);
+}
+
+// Function to calculate the average of the data received
+uint32_t calc_average() {
+  uint32_t avg = 0;
+  for (auto &d : last_data) {
+    avg += d;
+  }
+  avg /= last_data.size();
+  return avg;
+}
+
+// Function to check if all peers are ready
+bool check_all_peers_ready() {
+  for (auto &peer : peers) {
+    if (!peer->peer_ready) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/* Callbacks */
+
+// Callback called when a new peer is found
+void register_new_peer(const esp_now_recv_info_t *info, const uint8_t *data, int len, void *arg) {
+  esp_now_data_t *msg = (esp_now_data_t *)data;
+  int priority = msg->priority;
+
+  if (priority == self_priority) {
+    Serial.println("ERROR! Device has the same priority as this device. Unsupported behavior.");
+    fail_reboot();
+  }
+
+  if (current_peer_count < ESPNOW_PEER_COUNT) {
+    Serial.printf("New peer found: " MACSTR " with priority %d\n", MAC2STR(info->src_addr), priority);
+    ESP_NOW_Network_Peer *new_peer = new ESP_NOW_Network_Peer(info->src_addr, priority);
+    if (new_peer == nullptr || !new_peer->begin()) {
+      Serial.println("Failed to create or register the new peer");
+      delete new_peer;
+      return;
+    }
+    peers.push_back(new_peer);
+    current_peer_count++;
+    if (current_peer_count == ESPNOW_PEER_COUNT) {
+      Serial.println("All peers have been found");
+      new_msg.ready = true;
+    }
+  }
+}
+
+
+
+
 
 
 XboxGamepadDevice *gamepad;
 
 bool buttonJoystickLeftDownPressed = false;
 
-BleCompositeHID compositeHID("P2f", "P2f", 26);
+BleCompositeHID compositeHID("P3f", "P3f", 26);
 /*
 //ESP32 P2
 BleCompositeHID compositeHID("P2c", "P2c", 22);
@@ -43,132 +219,12 @@ JoystickSensor joystickP2Direction   = JoystickSensor(6, 7, true, true);
 //JoystickSensor joystickP4Direction = JoystickSensor( 9, 10, false, false)
 //JoystickSensor joystickP5Direction = JoystickSensor(20, 19, false, false)
 
-
 JoystickButton joystickP2Button = JoystickButton(4, 5, false, false, 0);
 
 
 
-typedef struct struct_message {
-    int idPlayer;       // ID del dispositivo que envía (ej: 1, 2, 3)
-    boolean isLeftSide;  // Algún valor de ejemplo
-    boolean isUpSide;  // Algún valor de ejemplo
-    int value1;
-    int value2;
-    
-} struct_message;
-
-struct_message myData;
-
-esp_now_peer_info_t peerInfo;
-
-// ===================================================
-// === CALLBACKS DE ESP-NOW ===
-// ===================================================
-
-// Callback cuando los datos son enviados
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  Serial.print("\nUltimo Paquete Enviado a: ");
-  for (int i = 0; i < 6; i++) {
-    Serial.printf("%02X:", mac_addr[i]);
-  }
-  Serial.println(status == ESP_NOW_SEND_SUCCESS ? " Enviado Correctamente" : " Fallo al Enviar");
-  
-  
-}
-
-// Callback cuando los datos son recibidos
-void OnDataRecv(const esp_now_recv_info * info, const uint8_t *incomingData, int len) {
-  // Ahora, para obtener la dirección MAC del emisor, la lees de la estructura 'info'
-  Serial.println("llego mensaje");
-  const uint8_t * mac_addr = info->des_addr; // Lee la MAC del emisor desde la estructura info
-
-  Serial.print("\nMensaje Recibido de: ");
-   for (int i = 0; i < 6; i++) {
-    Serial.printf("%02X:", mac_addr[i]); // Usa la MAC obtenida de info
-  }
-
-  struct_message receivedData;
-  memcpy(&receivedData, incomingData, sizeof(receivedData));
-
-  Serial.printf(" Tamano: %d bytes\n", len);
-  Serial.printf("  ID del Emisor: %d \n", receivedData.idPlayer); // El ID viene en los datos, no en la info de recepcion
-  //Serial.printf("  Valor: %f \n", receivedData.value);
-  //Serial.printf("  Texto: %s \n", receivedData.text);
-
-  // Opcionalmente, puedes acceder a RSSI u otra info:
-  // Serial.printf("  RSSI: %d dBm\n", info->rssi);
-}
-
-/* Classes */
-
-// Creating a new class that inherits from the ESP_NOW_Peer class is required.
-/*
-class ESP_NOW_Broadcast_Peer : public ESP_NOW_Peer {
-public:
-  // Constructor of the class using the broadcast address
-  ESP_NOW_Broadcast_Peer(uint8_t channel, wifi_interface_t iface, const uint8_t *lmk) : ESP_NOW_Peer(ESP_NOW.BROADCAST_ADDR, channel, iface, lmk) {}
-
-  // Destructor of the class
-  ~ESP_NOW_Broadcast_Peer() {
-    remove();
-  }
-
-  // Function to properly initialize the ESP-NOW and register the broadcast peer
-  bool begin() {
-    if (!ESP_NOW.begin() || !add()) {
-      log_e("Failed to initialize ESP-NOW or register the broadcast peer");
-      return false;
-    }
-    return true;
-  }
-
-  // Function to send a message to all devices within the network
-  bool send_message(const uint8_t *data, size_t len) {
-    if (!send(data, len)) {
-      log_e("Failed to broadcast message");
-      return false;
-    }
-    return true;
-  }
-};
-
-uint32_t msg_count = 0;
-// Create a broadcast peer object
-ESP_NOW_Broadcast_Peer broadcast_peer(ESPNOW_WIFI_CHANNEL, WIFI_IF_STA, NULL);
-
-//BleGamepad bleGamepad;
-//BleGamepadConfiguration bleGamepadConfig;  
- */ 
 
 
-// ===================================================
-// === FUNCION DE ENVIO AUXILIAR ===
-// ===================================================
-// Esta función ayuda a enviar datos a un peer especifico
-void sendDataToPeer(uint8_t *peer_mac) {
-
-    myData.idPlayer = 2;
-    myData.isLeftSide = true;
-    myData.isUpSide = false;
-    myData.value1 = 321;
-    myData.value2 = 123;
-    
-    esp_err_t result = esp_now_send(peer_mac, (uint8_t *) &myData, sizeof(myData));
-
-    if (result == ESP_OK) {
-        Serial.print("Enviando datos a MAC: ");
-        for (int i = 0; i < 6; i++) {
-            Serial.printf("%02X:", peer_mac[i]);
-        }
-        Serial.println(" - en proceso..."); // El resultado final se ve en OnDataSent
-    } else {
-        Serial.print("Error al enviar datos a MAC: ");
-         for (int i = 0; i < 6; i++) {
-            Serial.printf("%02X:", peer_mac[i]);
-        }
-        Serial.println(" - Fallo.");
-    }
-}
 
 void setup() {
 
@@ -211,6 +267,9 @@ void setup() {
   joystickP2Button.init(gamepad);
   joystickP2Button.setCallbackDirection(callbackDetectButtonsJoystick);
 
+  //ESP32
+  uint8_t self_mac[6];
+
   // Initialize the Wi-Fi module
   WiFi.mode(WIFI_STA);
   WiFi.setChannel(ESPNOW_WIFI_CHANNEL);
@@ -218,65 +277,36 @@ void setup() {
     delay(100);
   }
 
-  if (esp_now_init() != ESP_OK) {
-    Serial.println("Error al inicializar ESP-NOW");
-    ESP.restart(); // Reinicia si falla la inicialización
-    return;
-  }
-  Serial.println("ESP-NOW inicializado correctamente.");
-
+  Serial.println("ESP-NOW Network Example");
+  Serial.println("Wi-Fi parameters:");
+  Serial.println("  Mode: STA");
   Serial.println("  MAC Address: " + WiFi.macAddress());
   Serial.printf("  Channel: %d\n", ESPNOW_WIFI_CHANNEL);
 
-  esp_now_register_send_cb(OnDataSent);
-  esp_now_register_recv_cb(OnDataRecv);
-  Serial.println("Callbacks registrados.");
+  // Generate yhis device's priority based on the 3 last bytes of the MAC address
+  WiFi.macAddress(self_mac);
+  self_priority = self_mac[3] << 16 | self_mac[4] << 8 | self_mac[5];
+  Serial.printf("This device's priority: %lu\n", self_priority);
 
-  // ===================================================
-  // === REGISTRAR PEERS (CAMBIAR SEGUN EL DISPOSITIVO) ===
-  // ===================================================
-  // La configuración de peers cambia para cada dispositivo.
-  // Cada dispositivo debe registrar las MACs de los *otros dos*.
-  // --- REGISTRAR PEERS PARA D1 ---
-
-  // Configuración general de peerInfo
-  //memcpy(peerInfo.lmk, NULL, ESP_NOW_KEY_LEN); // Clave LMK (0 por defecto si no usas encriptacion)
-  memset(peerInfo.lmk, 0, ESP_NOW_KEY_LEN);
-  peerInfo.channel = 0; // Canal WiFi (0 = usa el canal actual, mejor si lo fijas explicitamente)
-  peerInfo.encrypt = false; // Deshabilitar encriptacion por ahora
-
-  if(PLAYER_ID != 2){
-    // Registrar D2
-    memcpy(peerInfo.peer_addr, MAC_P2, 6);
-    if (esp_now_add_peer(&peerInfo) != ESP_OK){
-      Serial.println("Fallo al añadir peer P2");
-      return;
-    }
-    Serial.println("Peer P2 registrado.");
-  }
-  
-  if(PLAYER_ID != 3){
-    // Registrar D3
-    memcpy(peerInfo.peer_addr, MAC_P3, 6);
-    if (esp_now_add_peer(&peerInfo) != ESP_OK){
-      Serial.println("Fallo al añadir peer P3");
-      return;
-    }
-    Serial.println("Peer P3 registrado.");
+  // Initialize the ESP-NOW protocol
+  if (!ESP_NOW.begin((const uint8_t *)ESPNOW_EXAMPLE_PMK)) {
+    Serial.println("Failed to initialize ESP-NOW");
+    fail_reboot();
   }
 
-  if(PLAYER_ID != 4){
-    // Registrar D4
-    memcpy(peerInfo.peer_addr, MAC_P4, 6);
-    if (esp_now_add_peer(&peerInfo) != ESP_OK){
-      Serial.println("Fallo al añadir peer P4");
-      return;
-    }
-    Serial.println("Peer P4 registrado.");
+  if (!broadcast_peer.begin()) {
+    Serial.println("Failed to initialize broadcast peer");
+    fail_reboot();
   }
-  
-  Serial.println("Peers registrados correctamente");
-  
+
+  // Register the callback to be called when a new peer is found
+  ESP_NOW.onNewPeer(register_new_peer, NULL);
+
+  Serial.println("Setup complete. Broadcasting own priority to find the master...");
+  memset(&new_msg, 0, sizeof(new_msg));
+  strncpy(new_msg.str, "Hello!", sizeof(new_msg.str));
+  new_msg.priority = self_priority;
+
 }
 
 void callbackDetectMovementJoystick(int positionX, int positionY) {
@@ -289,6 +319,7 @@ void callbackDetectMovementJoystick(int positionX, int positionY) {
   //sendMessage();
   Serial.printf("Heap libre: %d bytes\n", ESP.getFreeHeap());
   delay(10);//TODO: increase to 10 and put the delay do it with a counter time instead of delay()
+
 }
 
 void callbackDetectButtonsJoystick(int direction, boolean isPressed){
@@ -296,7 +327,7 @@ void callbackDetectButtonsJoystick(int direction, boolean isPressed){
   Serial.print(direction);
   Serial.print(" ,isPressed: ");
   Serial.println(isPressed); 
-  sendDataToPeer(MAC_P3);
+  //sendDataToPeer(MAC_P3);
   Serial.printf("Heap libre: %d bytes\n", ESP.getFreeHeap());
   delay(10);
 }
@@ -362,6 +393,86 @@ void loop() {
   joystickDirectionDetect();
   joysticksButtonsDetect();
 
+
+  if (!master_decided) {
+    // Broadcast the priority to find the master
+    if (!broadcast_peer.send_message((const uint8_t *)&new_msg, sizeof(new_msg))) {
+      Serial.println("Failed to broadcast message");
+    }
+
+    // Check if all peers have been found
+    if (current_peer_count == ESPNOW_PEER_COUNT) {
+      // Wait until all peers are ready
+      if (check_all_peers_ready()) {
+        Serial.println("All peers are ready");
+        // Check which device has the highest priority
+        master_decided = true;
+        uint32_t highest_priority = check_highest_priority();
+        if (highest_priority == self_priority) {
+          device_is_master = true;
+          Serial.println("This device is the master");
+        } else {
+          for (int i = 0; i < ESPNOW_PEER_COUNT; i++) {
+            if (peers[i]->priority == highest_priority) {
+              peers[i]->peer_is_master = true;
+              master_peer = peers[i];
+              Serial.printf("Peer " MACSTR " is the master with priority %lu\n", MAC2STR(peers[i]->addr()), highest_priority);
+              break;
+            }
+          }
+        }
+        Serial.println("The master has been decided");
+      } else {
+        Serial.println("Waiting for all peers to be ready...");
+      }
+    }
+  } else {
+    if (!device_is_master) {
+      // Send a message to the master
+      new_msg.count = sent_msg_count + 1;
+      new_msg.data = random(10000);
+      if (!master_peer->send_message((const uint8_t *)&new_msg, sizeof(new_msg))) {
+        Serial.println("Failed to send message to the master");
+      } else {
+        Serial.printf("Sent message to the master. Count: %lu, Data: %lu\n", new_msg.count, new_msg.data);
+        sent_msg_count++;
+      }
+
+      // Check if it is time to report to peers
+      if (sent_msg_count % REPORT_INTERVAL == 0) {
+        // Send a message to the peers
+        for (auto &peer : peers) {
+          if (!peer->peer_is_master) {
+            if (!peer->send_message((const uint8_t *)&new_msg, sizeof(new_msg))) {
+              Serial.printf("Failed to send message to peer " MACSTR "\n", MAC2STR(peer->addr()));
+            } else {
+              Serial.printf("Sent message \"%s\" to peer " MACSTR "\n", new_msg.str, MAC2STR(peer->addr()));
+            }
+          }
+        }
+      }
+    } else {
+      // Check if it is time to report to peers
+      if (recv_msg_count % REPORT_INTERVAL == 0) {
+        // Report average data to the peers
+        uint32_t avg = calc_average();
+        new_msg.data = avg;
+        for (auto &peer : peers) {
+          new_msg.count = sent_msg_count + 1;
+          if (!peer->send_message((const uint8_t *)&new_msg, sizeof(new_msg))) {
+            Serial.printf("Failed to send message to peer " MACSTR "\n", MAC2STR(peer->addr()));
+          } else {
+            Serial.printf(
+              "Sent message to peer " MACSTR ". Recv: %lu, Sent: %lu, Avg: %lu\n", MAC2STR(peer->addr()), recv_msg_count, new_msg.count, new_msg.data
+            );
+            sent_msg_count++;
+          }
+        }
+      }
+    }
+  }
+
+  delay(ESPNOW_SEND_INTERVAL_MS);
   /*
   int delayLoop = millis() - startLoop;
 
